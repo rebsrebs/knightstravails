fe326b1bf9dcd8e7b9ecdb3de6b9a9dbac92396e
“Rebecca <rebeccaschiffman@gmail.com>
2022-11-05 22:03:19 -0700

    Make Knight class and make gameBoard into a class

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
 
-const createBoard = function() {
-  let gameBoardArray = [];
-  for (let x = 1; x < 9; x++) {
-    for (let y = 1; y < 9; y++) {
-      const square = [x,y];
-      gameBoardArray.push(square);
+const gameBoard = class {
+
+  constructor() {
+    this.array = [];
+
+    for (let x = 1; x < 9; x++) {
+      for (let y = 1; y < 9; y++) {
+        const square = [x,y];
+        gameBoardArray.push(square);
+      }
     }
+    console.log(gameBoardArray);
   }
-  console.log(gameBoardArray);
+}
+
+
+
+
+const Knight = class {
+
+  constructor() {
+    this.position = [0,0];
+  }
+
+  //methods
+  knightMoves(array1,array2){
+    // find shortest path between two positions
+  }
 }
