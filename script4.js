91d331cc210105ac3929a839ea30d767dd95e9c8
“Rebecca <rebeccaschiffman@gmail.com>
2023-06-27 15:47:53 -0700

    Refactor in new file

--- script4.js
+++ script4.js
@@ -1,41 +1,9 @@
-//NODE CLASS
-class Node {
-  constructor(locationArray) {
-  this.locationArray = locationArray;
-  this.x = this.locationArray[0];
-  this.y = this.locationArray[1];
-  this.edgesList = [];
-  }
-
-  //METHODS
-  // connect(node) {
-  // this.edgesList.push(node);
-  // node.edgesList.push(this);
-  // }
-
-  getAdjacentNodes () {
-    return this.edgesList.map(edge => edge.locationArray);
-  }
-
-  // not sure how this works
-  isConnected(node) {
-    return this.edgesList.map(edge => edge.locationArray).indexOf(node.value) > -1;
-  }
-}
-//END NODE CLASS
-
-
-
-
-
-
-
-
 // Array to store all of the locations
 const vertices = [];
 
-// Makes 64 locations - one for each square of 8x8 chessboard
+// makeVertices - initializes 64 nodes - one for each square of 8x8 chessboard - each with a locationArray and pushes them into the vertices array.
 const makeVertices = function() {
+  console.log('running makeVertices');
   for (let x = 1; x < 9; x++) {
     for (let y = 1; y < 9; y++) {
       const vertex = new Node([x,y]);
@@ -45,11 +13,9 @@ const makeVertices = function() {
   return vertices;
 }
 
-// Create chessboard
-makeVertices();
-
 // Utility function to check if node's are already in each other's adjacency lists before pushing them
 const pushToEachOther = function(node, newX, newY) {
+  console.log('running pushToEachOther:');
   console.log(`newX is ${newX} and newY is ${newY}`);
   if ((0 < newY && newY < 9) && (0 < newX && newX < 9)) {
     var adjacentNode = vertices.find(item => item.x === newX && item.y === newY);
@@ -63,9 +29,10 @@ const pushToEachOther = function(node, newX, newY) {
     console.log('This is not on the board.');
   }
 }
-  
+
 // Specifically for a knight - utility function to find all adjacent nodes to the given node and put them in that node's edgeList
 const connect = function(node) {
+  console.log('running connect');
   // make variables
   let xP2 = parseFloat(node.x) + 2;
   let xM2 = parseFloat(node.x) - 2;
@@ -93,51 +60,56 @@ const connect = function(node) {
   //Left 2 Backward 1
   pushToEachOther(node, xM2, yM1);
 }
+// End connect function
 
 // Connect all board locations from vertices array by filling out adjacency lsits
 const makeConnections = function() {
+  console.log('running make Connections');
   vertices.forEach(connect);
 }
+
+//NODE CLASS
+class Node {
+  constructor(locationArray) {
+  this.locationArray = locationArray;
+  this.x = this.locationArray[0];
+  this.y = this.locationArray[1];
+  this.edgesList = [];
+  }
+}
+//END NODE CLASS
 
 
 // GAMEBOARD CLASS
 class GameBoard {
-
   // put vertices array here
   constructor (nodes) {
     this.nodes = [...nodes]; // ES6 spread operator
   }
-
-  addToGraph(node) {
-    this.nodes.push(node);
-  }
 }
 
 
+class Knight {
+  constructor(array) {
+    this.currentLocation = array;
+  }
 
+  //Methods
+  knightMoves (destination) {
+    console.log('Running knightMoves function');
 
+    //find which node matches the array
+    let currentNode =  vertices.find(item => item.locationArray[0] === this.currentLocation[0] && item.locationArray[1] === this.currentLocation[1]);
+    let endingNode = vertices.find(item => item.locationArray[0] === destination[0] && item.locationArray[1] === destination[1]);
 
+    let visited = [];
+    let queue = [];
+    queue.push(currentNode);
 
-// specifically for a knight
-//LEVEL ORDER FIND SHORTEST PATH
-const knightMoves = function(array1, array2) {
-  console.log('Running knightMoves function');
-
-  //find which node matches the array
-  //find the node in vertices that matches array1 - the starting node
-  let currentNode =  vertices.find(item => item.locationArray[0] === array1[0] && item.locationArray[1] === array1[1]);
-  // find the node in vertices that matches array 2 - the ending node
-  let endingNode = vertices.find(item => item.locationArray[0] === array2[0] && item.locationArray[1] === array2[1]);
-
-  let visited = [];
-  let queue = [];
-  //push the starting node into hte queue
-  queue.push(currentNode);
-
-  // while the queue has anything in it
-  while (queue.length > 0) {
-    console.log('queue[0]:');
-    console.log(queue[0]);
+    // while the queue has anything in it
+    while (queue.length > 0) {
+      console.log('queue[0]:');
+      console.log(queue[0]);
 
       // BASE CASE
       // if the first thing in the queue is adjacent to endingNode
@@ -145,9 +117,7 @@ const knightMoves = function(array1, array2) {
         // push that to done
         // push ending node to done
         console.log('Base case!');
-        // push the most recent one into visited
         visited.push(queue.shift());  
-        // push ending node into visited
         visited.push(endingNode);
         let moves = visited.map(node => node.locationArray);
         console.log(`Destination reached in ${moves.length - 1} moves! Here's your path:`)
@@ -169,9 +139,14 @@ const knightMoves = function(array1, array2) {
         console.log('About to push first in queue to visited')
         visited.push(queue.shift());    
       }
-  } //end while
+    }
+  }
 }
+// end knight class
 
-
-let chessBoard = new GameBoard(vertices);
+makeVertices();
 makeConnections();
+let chessBoard = new GameBoard(vertices);
+let theKnight = new Knight([1,1]);
+console.log('theKnight.currentLocation:')
+console.log(theKnight.currentLocation);
